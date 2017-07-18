fe790e3c91f9c27b922b09857033028c08a66692
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-18 13:04:16 -0700

    Made changes to routes

--- server.py
+++ server.py
@@ -18,7 +18,8 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html><html>Hi! This is the home page.<br>Go to the 
+    <a href="/hello">Hello Page</a></html>"""
 
 
 @app.route('/hello')
@@ -35,8 +36,31 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <select name="compliments">
+            <option value="awesome">Awesome</option>
+            <option value="terrific">Terrific</option>
+            <option value="fantastic">Fantastic</option>
+            <option value="Neato">Neato</option
+            <option value="fabtabulous">fabtabulous</option>
+            <option value="woza">Woza</option>
+            <option value="oh-so-not-meh">Oh-so-not-meh</option>
+          </select>
           <input type="submit" value="Submit">
         </form>
+
+      <form action="/diss">
+        What's your name? <input type="text" name="person">
+          <select name="insults">
+            <option value="not_awesome">Not Awesome</option>
+            <option value="not_terrific">Not Terrific</option>
+            <option value="not_fantastic">Not Fantastic</option>
+            <option value="not_neato">Not Neato</option
+            <option value="not_fabtabulous">Not fabtabulous</option>
+            <option value="woza">not_woza</option>
+            <option value="not_oh-so-not-meh">Not Oh-so-not-meh</option>
+          </select>
+        <input type="submit" value="Submit">
+    </form>
       </body>
     </html>
     """
@@ -48,8 +72,8 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
+    compliment = request.args.get("compliments")
+    #compliment = choice(AWESOMENESS)
     return """
     <!doctype html>
     <html>
@@ -61,6 +85,27 @@ def greet_person():
       </body>
     </html>
     """.format(player=player, compliment=compliment)
+
+
+@app.route('/diss')
+def say_insults():
+
+    player = request.args.get("person")
+
+    insult = request.args.get("insults")
+    #compliment = choice(AWESOMENESS)
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>An Insult</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {insult}!
+      </body>
+    </html>
+    """.format(player=player, insult=insult)
+
 
 
 if __name__ == '__main__':
